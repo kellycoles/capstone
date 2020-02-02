a46dc0e9ddac030c0fa836293e44973c81709f60
kellycoles <kellycoles1@gmail.com>
2020-02-01 18:36:00 -0600

    removed nav on welcome page

--- src/components/authentication/welcome.js
+++ src/components/authentication/welcome.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-// import HomeNavBar from "../home-nav/HomeNavBar"
 import "./Welcome.css"
 
 
@@ -10,12 +9,8 @@ class Welcome extends Component {
     render() {
         return (
             <>
-                {/* <HomeNavBar />  */}
                 <div className="header-container">
                     <div className="header">
-                        {/* <picture className="logo--icon">
-                            <img src={require('../../icons/lg-logo.png')} alt="logo-icon" />
-                        </picture> */}
                         <h1 className="welcome">Welcome to The Maintenance Log<br />
                         <small>Your maintenance record keeper</small></h1>
                     </div>
